08e1bbfa061363801acc827cfe76c314a032fbc7
Maxime Roussin-Bélanger <maxime.roussinbelanger@gmail.com>
2016-03-30 19:56:07 -0400

    If slack token isn't the one configured don't answer

--- route/weather.ts
+++ route/weather.ts
@@ -1,12 +1,20 @@
 import {Weather} from "../common/openweathermap";
+import {SlackToken} from "../config/config";
+
 let weather = new Weather();
+
 export function getCurrentWeather(req, res) {
-    console.log(req);
-    weather.getCurrentWeather({
-        q: req.query.q || '',
-        id: req.query.id || '',
-        lat: req.query.lat || '',
-        lon: req.query.lon || '',
-        zip: req.query.zip || '',
-    }, res);
+    if (req.token !== SlackToken) {
+        weather.getCurrentWeather({
+            q: req.query.q || '',
+            id: req.query.id || '',
+            lat: req.query.lat || '',
+            lon: req.query.lon || '',
+            zip: req.query.zip || '',
+        }, res);
+    } else {
+        res.sendStatus(403);
+    }
+
+
 }
