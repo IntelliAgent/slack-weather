cde814e77b296ef99e56bd1baf3793ab4116f7f9
Maxime Roussin-Bélanger <maxime.roussinbelanger@gmail.com>
2016-03-31 00:03:11 -0400

    Adds reponse to no forecast

--- common/openweathermap.ts
+++ common/openweathermap.ts
@@ -4,6 +4,10 @@ import request = require('request');
 import qs = require('querystring');
 import weatherKey = require('../config/config');
 
+declare module String {
+    export var format:any;
+}
+
 var currentWeatherEndpoint = 'http://api.openweathermap.org/data/2.5/weather?';
 var forecastEndpoint = 'http://api.openweathermap.org/data/2.5/forecast?';
 var dailyForecastEndpoint = 'http://api.openweathermap.org/data/2.5/forecast/daily?';
@@ -20,13 +24,27 @@ export class Weather {
 
     private queryOpenWeatherAPI(url, res) {
         request({
-                uri: url + '&appid=' + weatherKey.WeatherKEY,
+                uri: url + '&units=metric&appid=' + weatherKey.WeatherKEY,
                 method: 'GET'
             },
             function (error, response, body) {
                 if (!error && response.statusCode === 200) {
-                    console.log(body);
-                    res.status(200).send(body);
+                    if (body.list) {
+
+                    } else {
+                        body = JSON.parse(body);
+                        var temp = body.main.temp;
+                        var humidity = body.main.humidity;
+                        var windSpeed = body.wind.speed;
+                        res.status(200).send({
+                            response_type: 'in_channel',
+                            text: `It\'s ${temp}C with ${humidity}% humidity and ${windSpeed}meter/sec winds`,
+                            attachments: [
+                                {thumb_url: "http://openweathermap.org/img/w/" + body.weather[0].icon + ".png"}
+                            ]
+                        });
+                    }
+
                 } else {
                     console.error(error, body);
                     res.status(response.statusCode).send(body);
